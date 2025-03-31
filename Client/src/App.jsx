237ca2a08cdeac438df9905fe15484563ebaeb2e
21Scoob <vgaleriu56@gmail.com>
2025-03-31 18:57:27 +0300

    Deleting Settings.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.jsx";
 import Stocks from "./pages/Stocks.jsx";
 import Budget from "./pages/Budget.jsx";
-import Settings from "./pages/Settings.jsx";
 import Reports from "./pages/Reports.jsx";
 
 export default function App() {
@@ -14,7 +13,6 @@ export default function App() {
         <Route path="/stocks" element={<Stocks />} />
         <Route path="/budget" element={<Budget />} />
         <Route path="/reports" element={<Reports />} />
-        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
